aa7bd3183179b5a0f6b4f4eda3a9d0c5c5be67f7
Zakant <zakant@gmx.de>
2020-03-20 19:38:21 +0100

    + Added math.net nuget package and class for rotation matrices.

---
+++ MksNet/Spartial/Rotation.cs
@@ -0,0 +1,21 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MksNet.Spartial
+{
+    /// <summary>
+    /// Provides methods to obtain rotation matrices and derivatives with respect to angles.
+    /// </summary>
+    public static class Rotation
+    {
+        public static Matrix<double> GetXYZ(double alpha, double beta, double gamma) => GetX(alpha) * GetY(beta) * GetZ(gamma);
+
+        public static Matrix<double> GetX(double alpha) => CreateMatrix.DenseOfArray(new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } });
+
+        public static Matrix<double> GetY(double beta) => CreateMatrix.DenseOfArray(new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } });
+
+        public static Matrix<double> GetZ(double gamma) => CreateMatrix.DenseOfArray(new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } });
+    }
+}
