ba4026fa5f821e46e2fb2661f5ebec94e77222f4
Zakant <zakant@gmx.de>
2020-03-21 14:33:25 +0100

    + Added mockup methods for derivates and AgressivInlining Attribute.

--- MksNet/Spartial/Rotation.cs
+++ MksNet/Spartial/Rotation.cs
@@ -1,6 +1,7 @@
 ﻿using MathNet.Numerics.LinearAlgebra;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace MksNet.Spartial
@@ -10,12 +11,34 @@ namespace MksNet.Spartial
     /// </summary>
     public static class Rotation
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Matrix<double> GetXYZ(double alpha, double beta, double gamma) => GetX(alpha) * GetY(beta) * GetZ(gamma);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Matrix<double> GetX(double alpha) => CreateMatrix.DenseOfArray(new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } });
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Matrix<double> GetY(double beta) => CreateMatrix.DenseOfArray(new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } });
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Matrix<double> GetZ(double gamma) => CreateMatrix.DenseOfArray(new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } });
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Matrix<double> GetAlphaDerivate(double alpha, double beta, double gamma)
+        {
+            return CreateMatrix.DenseOfArray(new double[,] { { }, { }, { } });
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Matrix<double> GetBetaDerivate(double alpha, double beta, double gamma)
+        {
+            return CreateMatrix.DenseOfArray(new double[,] { { }, { }, { } });
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Matrix<double> GetGammaDerivate(double alpha, double beta, double gamma)
+        {
+            return CreateMatrix.DenseOfArray(new double[,] { { }, { }, { } });
+        }
     }
 }
