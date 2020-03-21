09c138bd2f10344612e3757417b763b47191e650
Zakant <zakant@gmx.de>
2020-03-21 15:31:36 +0100

    * Changed matrix creation to reuse provided array

--- MksNet/Spartial/Rotation.cs
+++ MksNet/Spartial/Rotation.cs
@@ -15,13 +15,13 @@ namespace MksNet.Spartial
         public static Matrix<double> GetXYZ(double alpha, double beta, double gamma) => GetX(alpha) * GetY(beta) * GetZ(gamma);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Matrix<double> GetX(double alpha) => CreateMatrix.DenseOfArray(new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } });
+        public static Matrix<double> GetX(double alpha) => CreateMatrix.Dense(3, 3, new double[] { 1, 0, 0, 0, Math.Cos(alpha), Math.Sin(alpha), 0, -Math.Sin(alpha), Math.Cos(alpha) });
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Matrix<double> GetY(double beta) => CreateMatrix.DenseOfArray(new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } });
+        public static Matrix<double> GetY(double beta) => CreateMatrix.Dense(3, 3, new double[] { Math.Cos(beta), 0, -Math.Sin(beta), 0, 1, 0, Math.Sin(beta), 0, Math.Cos(beta) });
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Matrix<double> GetZ(double gamma) => CreateMatrix.DenseOfArray(new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } });
+        public static Matrix<double> GetZ(double gamma) => CreateMatrix.Dense(3, 3, new double[] { Math.Cos(gamma), Math.Sin(gamma), 0, -Math.Sin(gamma), Math.Cos(gamma), 0, 0, 1 });
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Matrix<double> GetAlphaDerivate(double alpha, double beta, double gamma)
