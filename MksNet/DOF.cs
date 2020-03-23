d71f2e57e952df2c8a6917bc8c01548c003ee316
SoenkeKinematics <soenke.matthiesen@egnition-hamburg.de>
2020-03-23 16:12:42 +0100

    weird git stuff

---
+++ MksNet/DOF.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MksNet
+{
+    /// <summary>
+    /// Enumeration of degrees of freedom.
+    /// </summary>
+    public enum DOF
+    {
+        /// <summary>
+        /// Translation degree of freedom along the x axis.
+        /// </summary>
+        X,
+        /// <summary>
+        /// Translation degree of freedom along the y axis.
+        /// </summary>
+        Y,
+        /// <summary>
+        /// Translation degree of freedom along the z axis.
+        /// </summary>
+        Z,
+        /// <summary>
+        /// Rotational degree of freedom around the x axis.
+        /// </summary>
+        RotX,
+        /// <summary>
+        /// Rotational degree of freedom around the y axis.
+        /// </summary>
+        RotY,
+        /// <summary>
+        /// Rotational degree of freedom around the z axis.
+        /// </summary>
+        RotZ
+    }
+}
---
+++ MksNet/Joints/Joint.cs
@@ -0,0 +1,31 @@
+﻿using MksNet.Elements;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MksNet.Joints
+{
+    /// <summary>
+    /// Represents a joint to connect two elements.
+    /// </summary>
+    public class Joint
+    {
+        /// <summary>
+        /// Base frame the joint connects to.
+        /// </summary>
+        public Frame BaseFrame { get; internal set; }
+
+        /// <summary>
+        /// Follower frame the joint connects to.
+        /// </summary>
+        public Frame FollowerFrame { get; internal set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IEnumerable<DOF> LockedDegreesOfFreedom { get; internal set; }
+
+        internal Joint()
+        { }
+    }
+}
