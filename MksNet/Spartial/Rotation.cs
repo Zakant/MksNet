814274b8762738931426f9b56f7f3decf8a18d7a
Zakant <zakant@gmx.de>
2020-03-23 15:03:53 +0100

    * Fixed wrong attributes

--- MksNet/Spartial/Rotation.cs
+++ MksNet/Spartial/Rotation.cs
@@ -41,37 +41,37 @@ namespace MksNet.Spartial
             return CreateMatrix.DenseOfArray(new double[,] { {-Math.Sin(gamma), -Math.Cos(gamma), 0}, {Math.Cos(gamma), -Math.Sin(gamma), 0}, {0, 0, 0} });
         }
 
-        [MethodImp1(MethodImplOptions.AgressiveInlining)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Matrix<double> GetAlphaTimeDerivativeOfPartial(double alpha, double dalpha_dt)
         {
             return CreateMatrix.DenseOfArray(new double[,] { {0, 0, 0}, {0, -Math.Cos(alpha), Math.Sin(alpha)}, {0, -Math.Sin(alpha), -Math.Cos(alpha)} }) * dalpha_dt;
         }
 
-        [MethodImp1(MethodImplOptions.AgressiveInlining)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Matrix<double> GetBetaTimeDerivativeOfPartial(double beta, double dbeta_dt)
         {
             return CreateMatrix.DenseOfArray(new double[,] { {-Math.Cos(beta), 0, -Math.Sin(beta)}, {0, 0, 0}, {Math.Sin(beta), 0, -Math.Cos(beta)} }) * dbeta_dt;
         }
 
-        [MethodImp1(MethodImplOptions.AgressiveInlining)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Matrix<double> GetGammaTimeDerivativeOfPartial(double gamma, double dgamma_dt)
         {
             return CreateMatrix.DenseOfArray(new double[,] { {-Math.Cos(gamma), Math.Sin(gamma), 0}, {-Math.Sin(gamma), -Math.Cos(gamma), 0}, {0, 0, 0} }) * dgamma_dt;
         }
 
-        [MethodImp1(MethodImplOptions.AgressiveInlining)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Matrix<double> GetAlphaTotalTimeDerivative(double alpha, double beta, double gamma, double dalpha_dt, double dbeta_dt, double dgamma_dt)
         {
             return GetGammaPartialDerivate(gamma) * dgamma_dt * GetY(beta) * GetAlphaPartialDerivate(alpha) + GetZ(gamma) * GetBetaPartialDerivate(beta) * dbeta_dt * GetAlphaPartialDerivate(alpha) + GetZ(gamma) * GetY(beta) * GetAlphaTimeDerivativeOfPartial(alpha, dalpha_dt);
         }
 
-        [MethodImp1(MethodImplOptions.AgressiveInlining)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Matrix<double> GetBetaTotalTimeDerivative(double alpha, double beta, double gamma, double dalpha_dt, double dbeta_dt, double dgamma_dt)
         {
             return GetGammaPartialDerivate(gamma) * dgamma_dt * GetBetaPartialDerivate(beta) * GetX(alpha) * GetZ(gamma) * GetBetaTimeDerivativeOfPartial(beta, dbeta_dt) * GetX(alpha) + GetZ(gamma) * GetBetaPartialDerivate(beta) * GetAlphaPartialDerivate(alpha) * dalpha_dt;
         }
 
-        [MethodImp1(MethodImplOptions.AgressiveInlining)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Matrix<double> GetGammaTotalTimeDerivative(double alpha, double beta, double gamma, double dalpha_dt, double dbeta_dt, double dgamma_dt)
         {
             return GetGammaTimeDerivativeOfPartial(gamma, dgamma_dt) * GetY(beta) * GetX(alpha) + GetGammaPartialDerivate(gamma) * GetBetaPartialDerivate(beta) * dbeta_dt * GetX(alpha) + GetGammaPartialDerivate(gamma) * GetY(beta) * GetAlphaPartialDerivate(alpha) * dalpha_dt;
