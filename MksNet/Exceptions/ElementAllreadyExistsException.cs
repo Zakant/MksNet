c6a03d019c16d2fa710f31a71b02d21a553b0588
Zakant <zakant@gmx.de>
2020-03-25 11:11:44 +0100

    + Added exception types for duplicate type names.

---
+++ MksNet/Exceptions/ElementAllreadyExistsException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MksNet.Exceptions
+{
+    /// <summary>
+    /// The exception that is thrown when the element type allready exists within the factory.
+    /// </summary>
+    [Serializable]
+    public class ElementAllreadyExistsException : Exception
+    {
+        public ElementAllreadyExistsException() { }
+        public ElementAllreadyExistsException(string message) : base(message) { }
+        public ElementAllreadyExistsException(string message, Exception inner) : base(message, inner) { }
+        protected ElementAllreadyExistsException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ MksNet/Exceptions/JointAllreadyExistsException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MksNet.Exceptions
+{
+    /// <summary>
+    /// The exception that is thrown when the joint type allready exists within the factory.
+    /// </summary>
+    [Serializable]
+    public class JointAllreadyExistsException : Exception
+    {
+        public JointAllreadyExistsException() { }
+        public JointAllreadyExistsException(string message) : base(message) { }
+        public JointAllreadyExistsException(string message, Exception inner) : base(message, inner) { }
+        protected JointAllreadyExistsException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
