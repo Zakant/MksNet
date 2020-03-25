656b3c2c41f03e673280f32f2827b5c90896bfca
Zakant <zakant@gmx.de>
2020-03-25 10:42:12 +0100

    + Added exception classes.

---
+++ MksNet/Exceptions/ElementNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MksNet.Exceptions
+{
+
+    /// <summary>
+    /// The exception that is thrown when the requested element type can not be found.
+    /// </summary>
+    [Serializable]
+    public class ElementNotFoundException : Exception
+    {
+        public ElementNotFoundException() { }
+        public ElementNotFoundException(string message) : base(message) { }
+        public ElementNotFoundException(string message, Exception inner) : base(message, inner) { }
+        protected ElementNotFoundException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ MksNet/Exceptions/JointNotFoundException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MksNet.Exceptions
+{
+    /// <summary>
+    /// The exception that is thrown when the requested joint type can not be found.
+    /// </summary>
+    [Serializable]
+    public class JointNotFoundException : Exception
+    {
+        public JointNotFoundException() { }
+        public JointNotFoundException(string message) : base(message) { }
+        public JointNotFoundException(string message, Exception inner) : base(message, inner) { }
+        protected JointNotFoundException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
