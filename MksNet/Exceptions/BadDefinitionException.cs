d5ac9c7a11963ce50899c81ef335d07df183d3fa
Zakant <zakant@gmx.de>
2020-03-26 14:29:19 +0100

    + Added exception for bad formed definition files.

---
+++ MksNet/Exceptions/BadDefinitionException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MksNet.Exceptions
+{
+
+    /// <summary>
+    /// The exception that is thrown when a malformed or bad definition is encountered.
+    /// </summary>
+    [Serializable]
+    public class BadDefinitionException : Exception
+    {
+        public BadDefinitionException() { }
+        public BadDefinitionException(string message) : base(message) { }
+        public BadDefinitionException(string message, Exception inner) : base(message, inner) { }
+        protected BadDefinitionException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
