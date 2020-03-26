e89c553bd72e9088e90470263027705253c08f39
Zakant <zakant@gmx.de>
2020-03-26 14:28:48 +0100

    * Fixed name and argument type for matrix and vector operations.

---
+++ MksNet/Elements/Xml/Operations/Vector/VectorOperation.cs
@@ -0,0 +1,21 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using MksNet.Elements.Xml.Operations.Scalar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MksNet.Elements.Xml.Operations.Vector
+{
+    internal class VectorOperation : IVectorOperation
+    {
+        private List<IScalarOperation> vector;
+
+        internal VectorOperation(IEnumerable<IScalarOperation> scalars)
+        {
+            vector = scalars.ToList();
+        }
+
+        public Vector<double> Resolve(ElementParameter parameter) => CreateVector.DenseOfEnumerable(vector.Select(x => x.Resolve(parameter)));
+    }
+}
