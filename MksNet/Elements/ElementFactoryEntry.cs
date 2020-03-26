09e4e037c503fcd646ffcbf871b948a7254d85f4
Zakant <zakant@gmx.de>
2020-03-26 15:17:47 +0100

    * Improved factory storage classes.

--- MksNet/Elements/ElementFactoryEntry.cs
+++ MksNet/Elements/ElementFactoryEntry.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MksNet.Elements.Xml.Operations.Matrix;
+using MksNet.Elements.Xml.Operations.Scalar;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,20 @@ namespace MksNet.Elements
 {
     internal class ElementFactoryEntry
     {
+
+        internal string Name;
+
+        internal string Author;
+
+        internal string Description;
+
+        internal string Url;
+
+
+        internal IScalarOperation MassOperation;
+
+        internal IMatrixOperation InertiaOperation;
+
+        internal List<ElementFactoryEntryFrame> Frames;
     }
 }
