b46ba07165685022e1fa2d11a7f205ea76869bcb
Zakant <zakant@gmx.de>
2020-03-26 14:41:03 +0100

    + Added operations for trigonometric functions and to convert angle representations.

---
+++ MksNet/Elements/Xml/Operations/Scalar/Rad2DegOperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MksNet.Elements.Xml.Operations.Scalar
+{
+    internal class Rad2DegOperation : IScalarOperation
+    {
+        private IScalarOperation operation;
+
+        internal Rad2DegOperation(IScalarOperation operation)
+        {
+            this.operation = operation;
+        }
+
+        public double Resolve(ElementParameter parameter) => 180.0 / Math.PI * operation.Resolve(parameter);
+    }
+}
