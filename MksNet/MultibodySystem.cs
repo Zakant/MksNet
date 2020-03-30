88b7a7d6d5164cf99772c908a48396f69ec90090
SoenkeKinematics <soenke.matthiesen@egnition-hamburg.de>
2020-03-30 18:42:50 +0200

    *MultibodySystem: Added property for NumberOfElements

--- MksNet/MultibodySystem.cs
+++ MksNet/MultibodySystem.cs
@@ -6,5 +6,9 @@ namespace MksNet
 {
     public class MultibodySystem
     {
+        /// <summary>
+        /// The number of elements in the system
+        /// </summary>
+        public int NumberOfElements { get; private set; }
     }
 }
