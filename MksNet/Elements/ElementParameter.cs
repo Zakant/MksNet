187b7bad2904cf6184d5afa46e32b8906fc80b47
Zakant <zakant@gmx.de>
2020-03-31 09:52:33 +0200

    + Added ElementParameter class.

---
+++ MksNet/Elements/ElementParameter.cs
@@ -0,0 +1,88 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MksNet.Elements
+{
+    /// <summary>
+    /// Contains parameters used for <see cref="Element"/> creation.
+    /// </summary>
+    public class ElementParameter
+    {
+        /// <summary>
+        /// Empty parameter instace.
+        /// </summary>
+        public static ElementParameter Empty { get; } = new ElementParameter();
+
+        private Dictionary<string, double> scalarParameters = new Dictionary<string, double>();
+
+        private Dictionary<string, Vector<double>> vectorParameters = new Dictionary<string, Vector<double>>();
+
+        private Dictionary<string, Matrix<double>> matrixParameters = new Dictionary<string, Matrix<double>>();
+
+        /// <summary>
+        /// Add a new scalar parameter.
+        /// </summary>
+        /// <param name="name">Name of the parameter.</param>
+        /// <param name="value">Scalar parameter value.</param>
+        public void Add(string name, double value) => scalarParameters.Add(name, value);
+
+        /// <summary>
+        /// Add a new vector parameter.
+        /// </summary>
+        /// <param name="name">Name of the parameter.</param>
+        /// <param name="value">Vector parameter value.</param>
+        public void Add(string name, Vector<double> value) => vectorParameters.Add(name, value);
+
+        /// <summary>
+        /// Add a new matrix parameter.
+        /// </summary>
+        /// <param name="name">Name of the parameter.</param>
+        /// <param name="value">Matrix parameter value.</param>
+        public void Add(string name, Matrix<double> value) => matrixParameters.Add(name, value);
+
+        /// <summary>
+        /// Check if a scalar parameter with the given name is present.
+        /// </summary>
+        /// <param name="name">Name of the parameter.</param>
+        /// <returns>True if the parameter is present, otherwise false.</returns>
+        public bool HasScalar(string name) => scalarParameters.ContainsKey(name);
+
+        /// <summary>
+        /// Check if a vector parameter with the given name is present.
+        /// </summary>
+        /// <param name="name">Name of the parameter.</param>
+        /// <returns>True if the parameter is present, otherwise false.</returns>
+        public bool HasVector(string name) => vectorParameters.ContainsKey(name);
+
+        /// <summary>
+        /// Check if a matrix parameter with the given name is present.
+        /// </summary>
+        /// <param name="name">Name of the parameter.</param>
+        /// <returns>True if the parameter is present, otherwise false.</returns>
+        public bool HasMatrix(string name) => matrixParameters.ContainsKey(name);
+
+        /// <summary>
+        /// Get a scalar parameter with the given <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">Name of the parameter.</param>
+        /// <returns>The scalar value.</returns>
+        public double GetScalar(string name) => scalarParameters[name];
+
+        /// <summary>
+        /// Get a vector parameter with the given <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">Name of the parameter.</param>
+        /// <returns>The vector value.</returns>
+        public Vector<double> GetVector(string name) => vectorParameters[name];
+
+        /// <summary>
+        /// Get a matrix parameter with the given <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">Name of the parameter.</param>
+        /// <returns>The matrix value.</returns>
+        public Matrix<double> GetMatrix(string name) => matrixParameters[name];
+
+    }
+}
