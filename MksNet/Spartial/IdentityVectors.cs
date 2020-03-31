bfc5456c091e93907e058f6ea40bcce59ce8426d
Zakant <zakant@gmx.de>
2020-03-31 09:42:00 +0200

    * Changed identity vectors to properties.

--- MksNet/Spartial/IdentityVectors.cs
+++ MksNet/Spartial/IdentityVectors.cs
@@ -11,27 +11,18 @@ namespace MksNet.Spartial
         /// Identity Vector in x-Direction
         /// </summary>
         /// <returns></returns>
-        public static Vector<double> xAxis()
-        {
-            return CreateVector.Dense<double>(new double[] { 1,0,0});
-        }
+        public static Vector<double> X { get; } = CreateVector.Dense<double>(new double[] { 1, 0, 0 });
 
         /// <summary>
         /// Identity Vector in y-Direction
         /// </summary>
         /// <returns></returns>
-        public static Vector<double> yAxis()
-        {
-            return CreateVector.Dense<double>(new double[] { 0, 1, 0});
-        }
+        public static Vector<double> Y { get; } = CreateVector.Dense<double>(new double[] { 0, 1, 0});
 
         /// <summary>
         /// Identity Vector in z-Direction
         /// </summary>
         /// <returns></returns>
-        public static Vector<double> zAxis()
-        {
-            return CreateVector.Dense<double>(new double[] { 0, 0, 1});
-        }
+        public static Vector<double> Z { get; } = CreateVector.Dense<double>(new double[] { 0, 0, 1 });
     }
 }
