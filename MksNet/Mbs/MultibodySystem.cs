cdbf18fcc63e52af0d7a502c91dc7a96a78db1bb
SoenkeKinematics <soenke.matthiesen@egnition-hamburg.de>
2020-04-01 20:26:09 +0200

    *MultibodySystem: property for gravitational constant

--- MksNet/Mbs/MultibodySystem.cs
+++ MksNet/Mbs/MultibodySystem.cs
@@ -6,6 +6,14 @@ namespace MksNet.Mbs
 {
     public class MultibodySystem
     {
+        /// <summary>
+        /// The number of elements in the system
+        /// </summary>
         public int NumberOfElements { get; private set; }
+
+        /// <summary>
+        /// The gravitational constant
+        /// </summary>
+        public double GravitationalConstant { get; private set; } = -9.81;
     }
 }
