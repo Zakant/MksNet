676b230a48ca13857435ffbdeb34b57020a34489
Zakant <zakant@gmx.de>
2020-04-02 11:18:14 +0200

    + Added api methods for loading a multibody system.

--- MksNet/Mbs/MultibodySystem.cs
+++ MksNet/Mbs/MultibodySystem.cs
@@ -16,5 +16,20 @@ namespace MksNet.Mbs
         /// The magnitude and direction of gravitation.
         /// </summary>
         public Vector<double> GravitationVector { get; private set; }
+
+
+        /// <summary>
+        /// Load a mutlibody system defined in the file given by <paramref name="filename"/>.
+        /// </summary>
+        /// <param name="filename">Path of the multibody definition file.</param>
+        /// <returns>Multibody system instance.</returns>
+        public static MultibodySystem LoadFromFile(string filename) => Load(System.IO.File.ReadAllText(filename));
+
+        /// <summary>
+        /// Load a multibody system definied by the string given as <paramref name="definition"/>.
+        /// </summary>
+        /// <param name="definition">Definition string for a multibody system.</param>
+        /// <returns>Multibody system instance.</returns>
+        public static MultibodySystem Load(string definition) => MultibodySystemParser.Parse(definition);
     }
 }
