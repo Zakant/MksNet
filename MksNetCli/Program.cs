8e71e1bb9a9f6324002008ddb00c9a17eb346326
Zakant <zakant@gmx.de>
2020-04-03 09:36:46 +0200

    + Added new projects.

---
+++ MksNetCli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MksNetCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
